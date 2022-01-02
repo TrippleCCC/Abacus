ddb6d4f718baa2f66c853e049d46eac15c9310a4
TrippleCCC <chukwurahnonso@gmail.com>
2022-01-02 11:32:31 -0600

    Implement HistoryItem component

---
+++ src/lib/HistoryItem.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+    export let expression: string;
+    export let result: string;
+</script>
+
+<div class="container">
+    <div class="expression"><span class="arrow">&gt</span> {expression}</div>
+    <div class="result">{result}</div>
+</div>
+
+<style>
+    .arrow {
+        color: #FFB800;
+    }
+
+    .expression {
+        font-size: 2em;
+        color: white;
+    }
+
+    .result {
+        font-size: 1.5rem;
+        color: #2AC327;
+    }
+
+    .container {
+        margin-top: 1rem;
+        margin-bottom: 1rem;
+        margin-left: .5rem;
+    }
+</style>
